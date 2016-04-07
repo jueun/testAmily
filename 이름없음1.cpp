553a2cd7079ace9078a5266e1da05ca7da9ccdd8
jueun <connflip@gmail.com>
2016-04-07 15:46:27 +0900

    Sample
    Sample code just looking local value.

---
+++ 이름없음1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+int a;
+
+int main() {
+     int b;
+     
+     printf("전역변수%d \n 지역변수 %d",a,b);
+     
+     getch();
+     return 0;  
+     }
